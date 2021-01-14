07a9427f879fe49200ab636c08bcd4a0c2dfcceb
EzequielJavaS <68125532+EzequielJavaS@users.noreply.github.com>
2021-01-14 16:17:11 +0100

    Contenido v1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+
+
 // selector
 let menu = document.querySelector(".hamburger");
 let menuppal = document.querySelector(".menuppal");
-
-
 
 
 // method
 const toggleMenu = (event) =>{
   menu.classList.toggle('is-active');
   menuppal.classList.toggle("is_active");
-  event.preventDefault();
+  // event.preventDefault();
 }
 
 // event
